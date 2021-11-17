3631170ffc378cc9feef6d274d91d26a31db819b
dhsj8405 <dhsj8405@naver.com>
2021-11-17 11:06:12 +0900

    객체 구조분해

--- javascript-practices/es6/ex06.js
+++ javascript-practices/es6/ex06.js
@@ -1,5 +1,5 @@
 /**
- * 6. 구조분해
+ * 6. 구조분해(Destructing)
  */
 
 // ex1 - basic
@@ -11,8 +11,61 @@ const user = {
 
 // let firstName = user.firstName;
 // let lastName = user.lastName;
-({firstName, lastName} = user);
-
-f = function(){}
-
+// ({firstName, lastName} = user); // 괄호치면 안은 표현식 안치면 그냥 {}: 블락 
+let {firstName, lastName} = user;
 console.log(firstName, lastName);
+
+// es02 - Default Value
+const goods = {
+    name: 'TV',
+    price: 10000,
+    stockCount: 30
+}
+
+let {name, price, countStock=0, stockCount=0} = goods;
+console.log(name,price,countStock, stockCount);
+
+// ex03 - 구조 분해해야 할 객체의 속성과 변수 이름이 다를 때
+const person = {
+    name: '안대혁',
+    country: 'korea'
+};
+
+const {name: fullName, country: place} = person;
+console.log(fullName, place);
+
+// ex04 - 내부 객체(Nested Object)의 구조 분해
+const student= {
+    name: '둘리',
+    age: 10,
+    score: {
+        math: 90,
+        korean: 70,
+        science: 100
+    }
+};
+
+const {
+    name: studentName, 
+    score:{
+        math=0, 
+        korean=0, 
+        science=0, 
+        music=0
+    }
+} = student;
+    
+console.log(`${studentName}의 성적
+수학: ${math}
+국어: ${korean}
+과학: ${science}
+음악: ${music}`);
+
+// ex05 - 함수의 파라미터
+const averageScore = function({name: studentName, score:{math=0, korean=0, science =0}}){
+    console.log(`${studentName}의 평균은 ${(math + korean + science )/3} 입니다.`)
+};
+
+averageScore(student);
+
+// ex06 - 배열 ...
