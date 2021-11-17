b8e4a9c81d6802be37d435717afe4912b18354ca
dhsj8405 <dhsj8405@naver.com>
2021-11-17 10:24:40 +0900

    es6 destructing #1

---
+++ javascript-practices/es6/ex06.js
@@ -0,0 +1,18 @@
+/**
+ * 6. 구조분해
+ */
+
+// ex1 - basic
+const user = {
+    firstName: '둘',
+    lastName: '리',
+    email: 'kickscar@gmail.com'
+};
+
+// let firstName = user.firstName;
+// let lastName = user.lastName;
+({firstName, lastName} = user);
+
+f = function(){}
+
+console.log(firstName, lastName);
